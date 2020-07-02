5023dbdeecba84c576b279e55275054e6bc3e19e
13952522076 <xuma@my.unt.edu>
2020-07-02 13:33:14 -0500

    updates

--- tools/test_single_image.py
+++ tools/test_single_image.py
@@ -22,7 +22,7 @@ import matplotlib.cm as cm
 def vis_seg(data, result, img_norm_cfg, data_id, colors, score_thr, save_dir):
     img_tensor = data['img'][0]
     print(data)
-    img_metas = data['img_meta'][0]
+    img_metas = data['img_metas'][0]
     imgs = tensor2imgs(img_tensor, **img_norm_cfg)
     assert len(imgs) == len(img_metas)
     class_names = get_classes('coco')
