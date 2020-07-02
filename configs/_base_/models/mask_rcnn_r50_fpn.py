5efa7724c046bad18468516604025975c12afcf6
13952522076 <xuma@my.unt.edu>
2020-07-02 01:24:48 -0500

    update

--- configs/_base_/models/mask_rcnn_r50_fpn.py
+++ configs/_base_/models/mask_rcnn_r50_fpn.py
@@ -1,7 +1,7 @@
 # model settings
 model = dict(
     type='MaskRCNN',
-    pretrained='torchvision://resnet50',
+    pretrained='modelzoo://resnet50',
     backbone=dict(
         type='ResNet',
         depth=50,
