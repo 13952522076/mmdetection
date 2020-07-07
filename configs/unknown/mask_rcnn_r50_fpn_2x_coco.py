a0cda3bc531cc8a170cf567d9c196902a1a80b8c
13952522076 <xuma@my.unt.edu>
2020-07-07 11:54:28 -0500

    update

--- configs/unknown/mask_rcnn_r50_fpn_2x_coco.py
+++ configs/unknown/mask_rcnn_r50_fpn_2x_coco.py
@@ -47,13 +47,13 @@ data = dict(
     val=dict(
         type=dataset_type,
         # ann_file=data_root + 'annotations/instance_validation_gts.json',
-        ann_file=data_root + 'annotations/instance_validation_split_gts.json',
+        ann_file=data_root + 'annotations/instance_validation_gts.json',
         img_prefix=data_root + '/images/validation/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
         # ann_file=data_root + 'annotations/instance_validation_gts.json',
-        ann_file=data_root + 'annotations/instance_validation_split_gts.json',
+        ann_file=data_root + 'annotations/instance_validation_gts.json',
         img_prefix=data_root + '/images/validation/',
         pipeline=test_pipeline))
 evaluation = dict(metric=['segm'])
