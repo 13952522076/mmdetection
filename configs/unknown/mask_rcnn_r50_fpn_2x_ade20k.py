e6716a7f672f0fa20beb339ba92897319664a1b2
Xu Ma <xuma@my.unt.edu>
2020-07-13 12:44:30 -0500

    workers =4

--- configs/unknown/mask_rcnn_r50_fpn_2x_ade20k.py
+++ configs/unknown/mask_rcnn_r50_fpn_2x_ade20k.py
@@ -166,7 +166,7 @@ test_pipeline = [
 ]
 data = dict(
     samples_per_gpu=8,
-    workers_per_gpu=8,
+    workers_per_gpu=4,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instance_training_gts.json',
