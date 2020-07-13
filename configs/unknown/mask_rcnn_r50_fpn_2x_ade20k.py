4d59fb159af4b761c5904a9f96dbf881202fce58
Xu Ma <xuma@my.unt.edu>
2020-07-13 12:34:36 -0500

    update scale 465

--- configs/unknown/mask_rcnn_r50_fpn_2x_ade20k.py
+++ configs/unknown/mask_rcnn_r50_fpn_2x_ade20k.py
@@ -142,7 +142,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='Resize', img_scale=(465, 465), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -153,7 +153,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(512, 512),
+        img_scale=(465, 465),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -165,8 +165,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
+    samples_per_gpu=4,
+    workers_per_gpu=4,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instance_training_gts.json',
@@ -185,4 +185,4 @@ data = dict(
         img_prefix=data_root + '/images/validation/',
         pipeline=test_pipeline))
 evaluation = dict(metric=['segm'])
-work_dir = './work_dirs/mask_rcnn_r50_fpn_2x_ade20k'
+work_dir = './work_dirs/mask_rcnn_r50_fpn_2x_ade20k_465'
