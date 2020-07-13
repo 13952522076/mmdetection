b5594dc2d9bb27bcd710265bc82085bf3bef66b3
Xu Ma <xuma@my.unt.edu>
2020-07-13 16:28:12 -0500

    samples and workers makes not effect to speed

--- configs/unknown/mask_rcnn_r50_fpn_2x_ade20k.py
+++ configs/unknown/mask_rcnn_r50_fpn_2x_ade20k.py
@@ -165,8 +165,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=8,
-    workers_per_gpu=4,
+    samples_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instance_training_gts.json',
