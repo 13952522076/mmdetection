936f737b47d1310e5fa9bbe11f3165a788cf16a6
Xu Ma <xuma@my.unt.edu>
2020-07-15 18:55:16 -0500

    update

--- configs/unknown/mask_rcnn_r50_fpn_2x_ade20k.py
+++ configs/unknown/mask_rcnn_r50_fpn_2x_ade20k.py
@@ -153,7 +153,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1600, 1000),
+        img_scale=(712, 600),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
